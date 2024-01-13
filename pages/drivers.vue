188565b9f21d2194e9937e4bd49a4b1e5e39da56
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2024-01-13 22:36:23 +0100

    initial commit

---
+++ pages/drivers.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+
+const { data: drivers } = await supabase.from('drivers').select()
+</script>
+
+<template>
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Location</th>
+        </tr>
+        <tr v-for="driver in drivers" :key="driver.id">
+            <td>{{ driver.name }}</td>
+            <td>{{ driver.location }}</td>
+        </tr>
+    </table> 
+</template>
---
+++ pages/tours.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const supabase = useSupabaseClient()
+
+const { data: tours } = await supabase.from('tours').select()
+</script>
+
+<template>
+    <table>
+        <tr>
+            <th>Customer Name</th>
+            <th>Shipment Date</th>
+            <th>Location from</th>
+            <th>Location to</th>
+            <th>Assigned Driver</th>
+        </tr>
+        <tr v-for="tour in tours" :key="tour.id">
+            <td>{{ tour.customer_name }}</td>
+            <td>{{ tour.shipment_date }}</td>
+            <td>{{ tour.location_from }}</td>
+            <td>{{ tour.location_to }}</td>
+            <td>{{ tour.assigned_driver_id }}</td>
+        </tr>
+    </table> 
+</template>
