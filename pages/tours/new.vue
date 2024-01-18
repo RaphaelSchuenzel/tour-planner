f50c00ec002ab46cd5f187da00a796d10516d59c
Raphael Schünzel <inbox+git@raphaelschuenzel.com>
2024-01-18 01:57:16 +0100

    add new tour page

---
+++ pages/tours/new.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+let isShowSlideover = ref(true)
+
+function closeSlideover () {
+    const router = useRouter()
+    router.push({ path: '/tours' })
+}
+</script>
+
+<template>
+    <USlideover
+        :modelValue="isShowSlideover"
+        @update:modelValue="closeSlideover"
+    >
+        <div class="flex justify-between items-center h-16 px-5 border-b border-gray-200 dark:border-gray-700">
+            <p>Create new Tour</p>
+        </div>
+
+        <div class="p-5">
+            <TourForm />
+        </div>
+    </USlideover>
+</template>
